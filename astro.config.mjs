b1664403d0654b53359cda8b6576a6d1b78b20c8
Efraim Munthe <efrayanglain@gmail.com>
2023-07-15 12:52:21 +0700

    add link tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image()]
+  site: "https://smkmetland.netlify.app",
+  integrations: [image()],
 });
