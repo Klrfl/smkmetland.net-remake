d97ca9a13630aa43e817c5f7805301a61a793572
Efraim Munthe <efrayanglain@gmail.com>
2023-10-19 20:01:58 +0700

    upgrade to astro v3, update image integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
-import { defineConfig } from 'astro/config';
-
-import image from "@astrojs/image";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [image()]
-});
+export default defineConfig({});
+
