05684ffb2caf5f54de314f7d57c74e9489ef6afb
Efraim Munthe <efrayanglain@gmail.com>
2024-04-06 09:55:07 +0700

    move posts endpoint into .env variable

--- src/scripts/consts.js
+++ src/scripts/consts.js
@@ -1,5 +1,4 @@
 const SITE_TITLE = "SMK Metland";
 const SITE_DESCRIPTION = "Situs web resmi SMK Metland.";
-const POSTS_ENDPOINT = "https://smkmetland.net/ppdb/wp-json/wp/v2/posts";
-// const POSTS_ENDPOINT = "https://yrhi.org/wp-json/wp/v2/posts";
+const POSTS_ENDPOINT = import.meta.env.POSTS_ENDPOINT;
 export { SITE_TITLE, SITE_DESCRIPTION, POSTS_ENDPOINT };
