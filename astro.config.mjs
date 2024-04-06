d0669095cea6828fc79e396a9313ca8e51690a8e
Efraim Munthe <efrayanglain@gmail.com>
2024-04-06 11:01:59 +0700

    add astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://smkmetland.pages.dev",
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
   redirects: {
     "/berita-sekolah/": "berita-sekolah/1",
   },
 });
+
